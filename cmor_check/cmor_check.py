32581cb8c6b86eed5d82ae2a7265fde73ec869ff
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-04-10 13:58:17 +0200

    initial cv check

--- cmor_check/cmor_check.py
+++ cmor_check/cmor_check.py
@@ -1,2 +1,27 @@
-def cmor_check():
+from . import log
+from .log import get_logger
+from .utils import read_json
+
+logger = get_logger(__name__)
+
+
+def check_cv(ds, cv_table):
+    report = {}
+    required_global_attributes = cv_table["required_global_attributes"]
+    logger.info(f"required global attributes: {required_global_attributes}")
+
+    global_attrs = ds.attrs
+    for attr in required_global_attributes:
+        report[attr] = global_attrs.get(attr) or "ERROR"
+
+    return report
+
+
+def cmor_check(ds, cv_table=None):
+    if cv_table:
+        cv_table = read_json(cv_table)
+        report = check_cv(ds, cv_table.get("CV") or cv_table)
+        for k, v in report.items():
+            lev = log.levels[v]
+            logger.log(lev, f"{k}: {v}")
     print("cmor check")
---
+++ cmor_check/utils.py
@@ -0,0 +1,18 @@
+import json
+
+import requests
+
+
+def read_json_url(url):
+    with requests.get(url) as r:
+        return r.json()
+
+
+def read_json(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+
+def write_json(filename, data):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
