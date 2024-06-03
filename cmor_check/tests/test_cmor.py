3435545fa300549a3f3a9acbe741487c85d201da
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-06-03 17:19:17 +0200

    fix

--- cmor_check/tests/test_cmor.py
+++ cmor_check/tests/test_cmor.py
@@ -5,7 +5,7 @@ from ..tables import CORDEX_CMIP6
 url = "https://raw.githubusercontent.com/euro-cordex/py-cordex-data/main/CORDEX/CMIP6/DD/EUR-12/GERICS/ERA5/evaluation/r1i1p1f1/REMO2020/v1/fx/orog/v20240529/orog_EUR-12_ERA5_evaluation_r1i1p1f1_GERICS_REMO2020_v1_fx.nc"
 
 
-def test_cmor(filename, cv):
+def test_cmor():
     ds = cordex_cmip6()
     cv = CORDEX_CMIP6("CV")
     return cmor_check(ds, cv)
