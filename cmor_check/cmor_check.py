22906d9578d29f2e7c870a951ab0a16c47610135
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-06-03 18:16:24 +0200

    added grid checks

--- cmor_check/cmor_check.py
+++ cmor_check/cmor_check.py
@@ -3,6 +3,7 @@ import xarray as xr
 from . import log
 from .log import get_logger
 from .utils import read_json
+import numpy as np
 
 logger = get_logger(__name__)
 
@@ -13,18 +14,60 @@ def check_cv(ds, cv_table):
     logger.info(f"required global attributes: {required_global_attributes}")
 
     for attr in required_global_attributes:
-        report[attr] = ds.attrs.get(attr) or "ERROR"
+        value = ds.attrs.get(attr)
+        if not value:
+            logger.error(f"missing required global attribute: {attr}")
+            report[attr] = "MISSING"
+        elif attr in cv_table.keys():
+            cv_values = cv_table[attr]
+            if isinstance(cv_values, dict):
+                cv_values = list(cv_values.keys())
+            if value not in cv_values:
+                message = f"value {value} of required global attribute {attr} is not in CV: {cv_values}"
+                logger.warning(message)
+                report[attr] = message
 
     return report
 
 
-def cmor_check(ds, cv_table=None):
+def _check_coord(coord, ref):
+    logger.info(f"Checking coordinate: {coord.name}")
+    close = np.allclose(coord, ref)
+    if not close:
+        logger.warning(f"Coordinate {coord.name} has wrong values!")
+    return close
+
+
+def check_cordex_grid(ds):
+    import cordex as cx
+    
+    domain_id = ds.cx.domain_id
+    logger.info(f"Found domain_id: {domain_id}")
+    dm = cx.cordex_domain(domain_id, mip_era="CMIP6", bounds=True)
+    
+    for axis in ['X', 'Y']:
+        _check_coord(ds.cf[axis], dm.cf[axis])    
+
+   
+def check_project(ds):
+    if "project_id" in ds.attrs:
+        project_id = ds.attrs["project_id"]
+        logger.info(f"Found project_id: {project_id}")
+        return "CORDEX-CMIP6"
+    if "activity_id" in ds.attrs:
+        logger.info(f"Found activity_id: {activity_id}")
+        return "CMIP6"
+
+
+def cmor_check(ds, cv_table=None, project=None):
+    if project is None:
+        project = check_project(ds)
     if cv_table:
         logger.info("Checking CV")
         report = check_cv(ds, cv_table.get("CV") or cv_table)
-        for k, v in report.items():
-            lev = log.levels[v]
-            logger.log(lev, f"{k}: {v}")
+    if project == "CORDEX-CMIP6":
+        check_cordex_grid(ds)
+    pass
 
 
 def check_file(filename, cv_table=None):
