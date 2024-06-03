6651ab952ffc1026b0f278ef2e153c0fd609f2a2
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-06-03 17:13:37 +0200

    pre-commit

--- cmor_check/tables.py
+++ cmor_check/tables.py
@@ -1,17 +1,19 @@
-
-
 from os import path as op
+
 from .utils import read_json
 
 CORDEX_CMIP6_URL = "https://raw.githubusercontent.com/WCRP-CORDEX/cordex-cmip6-cmor-tables/main/Tables/"
 
+
 def retrieve(url):
     import pooch
+
     # URL to one of Pooch's test files
-    filename = pooch.retrieve(url=url,
-                              known_hash=None,
-                              path="~/.cmor-check",
-                              )
+    filename = pooch.retrieve(
+        url=url,
+        known_hash=None,
+        path="~/.cmor-check",
+    )
     return filename
 
 
--- cmor_check/tests/test_cmor.py
+++ cmor_check/tests/test_cmor.py
@@ -1,12 +1,11 @@
-
 from ..cmor_check import cmor_check
 from ..datasets import cordex_cmip6
-
+from ..tables import CORDEX_CMIP6
 
 url = "https://raw.githubusercontent.com/euro-cordex/py-cordex-data/main/CORDEX/CMIP6/DD/EUR-12/GERICS/ERA5/evaluation/r1i1p1f1/REMO2020/v1/fx/orog/v20240529/orog_EUR-12_ERA5_evaluation_r1i1p1f1_GERICS_REMO2020_v1_fx.nc"
 
 
 def test_cmor(filename, cv):
     ds = cordex_cmip6()
-    cv = tables.CORDEX_CMIP6("CV")
-    checked = cmor_check(ds, cv)
+    cv = CORDEX_CMIP6("CV")
+    return cmor_check(ds, cv)
